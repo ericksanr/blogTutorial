38a06221b903be555bb30a88492d2125eab7946f
ericksanr <sistemasbarragan@oxschool.edu.mx>
2021-05-20 08:37:47 -0500

    Modify routing, 6.2 Resourceful Routing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+        @articles = Article.all #Take all the articles saved
+    end
+
+    def show
+        @article = Article.find (params[:id]) #seeks for the article selcted, by id
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root "articles#index"
   
   #Creates a route, ask for articles and sed them to index
-  get "/articles", to: "articles#index"
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to "articles#show"
+   
+  resources :articles  
+  #maps all the routes 
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
