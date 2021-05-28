f891d69b3b5249abe1329bb4289d4b91dddd019c
ericksanr <sistemasbarragan@oxschool.edu.mx>
2021-05-28 18:22:14 -0500

    Adding a Route for Comments 7.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to "articles#show"
    
-  resources :articles  
+  resources :articles do 
+    resources :comments
+  end
   #maps all the routes 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
