b980f829c1bec8699afdc94fc3c3c4dc6d998c83
ericksanr <sistemasbarragan@oxschool.edu.mx>
2021-06-05 00:14:43 -0500

    -

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   
   root "articles#index"
   
-  #Creates a route, ask for articles and sed them to index
-  #get "/articles", to: "articles#index"
-  #get "/articles/:id", to "articles#show"
-   
+  
   resources :articles do 
-    resources :comments
+    resources :comments #creates comments as a nested source within articles
   end
   #maps all the routes 
 end
